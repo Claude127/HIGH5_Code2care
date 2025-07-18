835e8a1626a1e271bdc4f3bbe2044a5f243acd89
Osiris-10 <131268904+Osiris-10@users.noreply.github.com>
2025-07-18 17:20:48 +0200

    Update hf_config.py
    updating model id link

--- dgh-platform/feedback/hf_config.py
+++ dgh-platform/feedback/hf_config.py
@@ -1,5 +1,5 @@
 # hf_config.py
 
 # Identifiant du modèle sur Hugging Face (public, donc pas besoin de token)
-HF_MODEL_ID = "https://huggingface.co/cardiffnlp/twitter-xlm-roberta-base-sentiment"
+HF_MODEL_ID = "genie10/feedback_patients"
 HF_TOKEN = None
